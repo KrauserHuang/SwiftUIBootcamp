d5d3603dc760c796309a55ec61f8975f2d0475a6
Krauser Huang <crescentdence@gmail.com>
2024-06-16 14:57:11 +0800

    Initial Commit

---
+++ SwiftUIBootcamp/SwiftUIBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampApp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Tai Chin Huang on 2024/6/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIBootcampTests/SwiftUIBootcampTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootcampTests.swift
+//  SwiftUIBootcampTests
+//
+//  Created by Tai Chin Huang on 2024/6/16.
+//
+
+import Testing
+@testable import SwiftUIBootcamp
+
+struct SwiftUIBootcampTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
