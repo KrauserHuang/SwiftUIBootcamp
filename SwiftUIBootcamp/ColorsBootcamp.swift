77a43f4ebddcbd73aa0ad405405e7b6ba2500e14
Krauser Huang <crescentdence@gmail.com>
2024-06-18 21:18:54 +0800

    [Feature] SwiftfulThinking SwiftUIBootcamp
    create project

---
+++ SwiftUIBootcamp/ColorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorsBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Tai Chin Huang on 2024/6/18.
+//
+
+import SwiftUI
+
+struct ColorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorsBootcamp()
+}
---
+++ SwiftUIBootcamp/ShapesBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ShapesBootcamp.swift
+//  SwiftUIBootcamp
+//
+//  Created by Tai Chin Huang on 2024/6/16.
+//
+
+import SwiftUI
+
+struct ShapesBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShapesBootcamp()
+}
