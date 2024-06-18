86cab6ff360ac67a193480bb5050fe905b4529b2
Krauser Huang <crescentdence@gmail.com>
2024-06-18 21:21:25 +0800

    [Feature] ColorsBootcamp #4
    create ColorsBootcamp file/CustomColor color set in asset

--- SwiftUIBootcamp/ColorsBootcamp.swift
+++ SwiftUIBootcamp/ColorsBootcamp.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct ColorsBootcamp: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        RoundedRectangle(cornerRadius: 25)
+            .fill(
+//                Color.primary
+//                Color(#colorLiteral(red: 0.01961897127, green: 0.2666915655, blue: 0.2861078382, alpha: 1))
+//                Color(#colorLiteral(red: 0.4974797368, green: 0.8199130297, blue: 0.9641874433, alpha: 1))
+//                Color(uiColor: .secondarySystemBackground)
+                Color("CustomColor")
+            )
+            .frame(width: 300, height: 200)
+//            .shadow(radius: 10)
+            .shadow(color: Color("CustomColor").opacity(0.3), radius: 10, x: 20, y: 20)
     }
 }
 
